62116a356de4bb152fbb4445edec0722a7c84e48
fjanphilip <fjanphilip9.jp@gmail.com>
2026-05-07 19:37:20 +0700

    feat: initialize axios api client with base url and bearer token support

--- app/lib/api.js
+++ app/lib/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://127.0.0.1:8000/api";
+// const API_BASE_URL = "http://127.0.0.1:8000/api";
+const API_BASE_URL = "https://costume-rent.mahesvara.engineer/api";
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
