b866a31c9888d5117f127545122412cf13ac5c01
fjanphilip <fjanphilip9.jp@gmail.com>
2026-06-08 21:09:25 +0700

    chore: configure Vercel preset in vite.config.js and remove framework property from vercel.json

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,14 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { vercelPreset } from "@vercel/remix/vite";
 
 export default defineConfig({
     plugins: [
         tailwindcss(),
         tsconfigPaths(),
         remix({
+            presets: [vercelPreset()],
             future: {
                 v3_fetcherPersist: true,
                 v3_relativeBoundaryComponents: true,
